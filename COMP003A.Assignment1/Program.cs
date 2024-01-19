e00689ab87b25c928112207e0924063ddcbaa9bc
bsaul <bsaul@SAUL>
2024-01-18 16:47:20 -0800

    Add project files.

---
+++ COMP003A.Assignment1/Program.cs
@@ -0,0 +1,120 @@
+﻿// single-line comment
+/* multi-line comment
+ * Author:  Saul Barajas
+ * Course:  COMP-003A-L01
+ * Purpose  Code for Assignment 1
+ */
+
+
+namespace COMP003A.Assignment1
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("100 Things About Me");
+            Console.WriteLine("I like to draw");
+            Console.WriteLine("I like Watermelon");
+            Console.WriteLine("I play volleyball");
+            Console.WriteLine("I am on a semi pro video game team");
+            Console.WriteLine("Favorite color is light blue");
+            Console.WriteLine("Favorite game genres are mmo/fps");
+            Console.WriteLine("I workout");
+            Console.WriteLine("I have ADHD");
+            Console.WriteLine("I can speak Spanish");
+            Console.WriteLine("I like to hangout with friends");
+            Console.WriteLine("Technology intrest me");
+            Console.WriteLine("I can built a PC");
+            Console.WriteLine("I own a PC");
+            Console.WriteLine("I've made 500$ from video games");
+            Console.WriteLine("I like chess");
+            Console.WriteLine("Im ok at chess");
+            Console.WriteLine("English is my worst subject");
+            Console.WriteLine("Science is my best subject");
+            Console.WriteLine("I have friends that live in Japan");
+            Console.WriteLine("I am a manager at Little Caesars");
+            Console.WriteLine("I have an android");
+            Console.WriteLine("Math makes my brain hurt");
+            Console.WriteLine("I like to drive fast");
+            Console.WriteLine("I like to swim");
+            Console.WriteLine("I like to go camping");
+            Console.WriteLine("I like walking at night");
+            Console.WriteLine("I have a horrible sleep schedule");
+            Console.WriteLine("I am a top 500 player on Overwatch 2");
+            Console.WriteLine("I been going to the gym for 1 year");
+            Console.WriteLine("I like mexican food");
+            Console.WriteLine("I have been to Mexico");
+            Console.WriteLine("I have been to New York");
+            Console.WriteLine("I have been to Chicago");
+            Console.WriteLine("I dont like the color yellow");
+            Console.WriteLine("I like sour candy");
+            Console.WriteLine("I’m a great listener");
+            Console.WriteLine("I am honest");
+            Console.WriteLine("I’m a fast learner");
+            Console.WriteLine("I have ok cooking skills");
+            Console.WriteLine("I appreciate the little things in life");
+            Console.WriteLine("I’m trustworthy");
+            Console.WriteLine("I’m great at learning new things");
+            Console.WriteLine("I tackle problems with confidence");
+            Console.WriteLine("I am patient");
+            Console.WriteLine("I love the movie “Interstellar”.");
+            Console.WriteLine("I like amusement parks");
+            Console.WriteLine("My favorite season is Fall");
+            Console.WriteLine("Favorite holiday is Halloween");
+            Console.WriteLine("I am big into horror");
+            Console.WriteLine("I have 2 dogs");
+            Console.WriteLine("I am a middle child");
+            Console.WriteLine("I have an older sister");
+            Console.WriteLine("I have a younger brother");
+            Console.WriteLine("I need glasses or ill crash into a wall");
+            Console.WriteLine("I cant drive at night without my glasses");
+            Console.WriteLine("Favorite drink is pink lemonade");
+            Console.WriteLine("As a child I was at the doctors alot for injuries");
+            Console.WriteLine("I like walking around in the cities at night");
+            Console.WriteLine("I like music");
+            Console.WriteLine("I google alot of stuff when I dont know the answer");
+            Console.WriteLine("I want to believe in second chances, but from what I have seen, they don’t usually yield the desired results.");
+            Console.WriteLine("I am always shocked by people who haven’t dealt with death or been to funerals.");
+            Console.WriteLine(" I have no tattoos");
+            Console.WriteLine("I plan to get a Geometric tattoo");
+            Console.WriteLine("I am always late even thought I try not to be");
+            Console.WriteLine("I lose track of time very easily");
+            Console.WriteLine("I lose things alot");
+            Console.WriteLine("When talking to someone if they tell me something ill say 'what or huh' even thought I heard what they said my brain is just processing it");
+            Console.WriteLine("I fidget alot");
+            Console.WriteLine("I have a caffeine addiction");
+            Console.WriteLine("I like teas");
+            Console.WriteLine("I like to read mangas");
+            Console.WriteLine("I like to read manwhas");
+            Console.WriteLine("I like to watch movies");
+            Console.WriteLine("I like to watch anime");
+            Console.WriteLine("My favorite anime is Jujutsu Kaisen");
+            Console.WriteLine("I have been playing Clash of Clans for 12 years now");
+            Console.WriteLine("I am broke");
+            Console.WriteLine("I like wearing hoodies");
+            Console.WriteLine("I don't like pictures");
+            Console.WriteLine("I’d like to live by the ocean");
+            Console.WriteLine("My friends call me an Instigator");
+            Console.WriteLine("I Instigate alot");
+            Console.WriteLine("I procrastinate");
+            Console.WriteLine("I oversleep alot ");
+            Console.WriteLine("I sleep late");
+            Console.WriteLine("My favorite icecream is Cookie dough");
+            Console.WriteLine("Favorite tea is Chai/green tea");
+            Console.WriteLine("Favorite mexican food is tacos");
+            Console.WriteLine("Favorite Korean food is Bulgogi");
+            Console.WriteLine("Favorite FPS game is The Finals");
+            Console.WriteLine("Favorite MMO game is BDO");
+            Console.WriteLine("Favorite fighting game is Mortal Kombat");
+            Console.WriteLine("Favorite Horror movie is It Follows");
+            Console.WriteLine("Favorite Horror game is Phasmophobia");
+            Console.WriteLine("I get side tracked all the time");
+            Console.WriteLine("I have friends that live in Japan");
+            Console.WriteLine("I have known my best friend for 16 years");
+            Console.WriteLine("I have never coded");
+            Console.WriteLine("I can adapt to problems");
+
+        }
+    }
+}
